171c0bf6d5f93bce66fd4649fd54ce199d007697
Lucas Oliveira <luksrn@gmail.com>
2013-04-21 21:03:53 -0300

    Criação de método modelo para login

--- src/main/java/br/ufrn/dimap/pubnote/ws/UserService.java
+++ src/main/java/br/ufrn/dimap/pubnote/ws/UserService.java
@@ -1,10 +1,13 @@
 package br.ufrn.dimap.pubnote.ws;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import br.ufrn.dimap.pubnote.domain.User;
+import br.ufrn.dimap.pubnote.ws.responses.LoginFailure;
 
 @Path("/user")
 public class UserService {
@@ -16,8 +19,27 @@ public class UserService {
 	 */	
 	@POST
 	@Path("/register")
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createUser(User user){
 		System.err.println("Recebendo request para " + user );
 		return Response.status(201).build();
+	}
+	
+	/**
+	 * curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d '{"username":"luksrn", "password":"senhaTopSecreta"}' http://localhost:8080/pubnote.server/rest/user/login
+	 * 
+	 * @param user
+	 * @return
+	 */
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON) 
+	public Response login(User user){
+		if( user.getUsername().equalsIgnoreCase("userTeste" ) ){
+			return Response.ok().build();
+		}
+		LoginFailure loginFailure = new LoginFailure();
+		loginFailure.setMessage("Invalid login or password.");		
+		return Response.status(401).entity(loginFailure).build();
 	}
 }
---
+++ src/main/java/br/ufrn/dimap/pubnote/ws/responses/LoginFailure.java
@@ -0,0 +1,16 @@
+package br.ufrn.dimap.pubnote.ws.responses;
+
+public class LoginFailure {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
