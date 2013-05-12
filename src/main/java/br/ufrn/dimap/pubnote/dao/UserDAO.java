165348d9a39c1956ee332c5643d0a93d99ad8a5d
judf <judf.oliveira@gmail.com>
2013-05-12 18:42:02 -0300

    Adicionando serviços de autenticação e recuperação de profile.

--- src/main/java/br/ufrn/dimap/pubnote/dao/UserDAO.java
+++ src/main/java/br/ufrn/dimap/pubnote/dao/UserDAO.java
@@ -4,6 +4,8 @@ import org.hibernate.Query;
 import org.hibernate.Transaction;
 
 import br.ufrn.dimap.pubnote.domain.EvaluationEntity;
+import br.ufrn.dimap.pubnote.domain.ProfileEntity;
+import br.ufrn.dimap.pubnote.domain.User;
 import br.ufrn.dimap.pubnote.domain.UserEntity;
 
 /*
@@ -11,8 +13,9 @@ import br.ufrn.dimap.pubnote.domain.UserEntity;
  */
 public class UserDAO extends DAO<UserEntity> 
 {
-	private final static String USER_BY_USERNAME = "from punotes_user u where u.username = :username";
-
+	private final static String USER_BY_USEREMAIL = "from pubnotes_user u where u.useremail = :useremail";
+	private final static String PROFILE_BY_USERID = "from profile p, pubnotes_user u where p.id = :u.profile.id";
+	
 	@Override
 	public void persist(UserEntity obj) {
 		session.persist(obj);
@@ -24,13 +27,13 @@ public class UserDAO extends DAO<UserEntity>
 		return entity;
 	}
 	
-	public UserEntity loadByUsername(String username) {
-		Query query = session.createQuery(USER_BY_USERNAME);
-		query.setParameter("username", username);
+	public UserEntity loadByUsername(String useremail) {
+		Query query = session.createQuery(USER_BY_USEREMAIL);
+		query.setParameter("useremail", useremail);
 		UserEntity entity = (UserEntity) query.uniqueResult();
 		return entity;
 	}
-
+	
 	public void update(UserEntity obj) {
 		session.update(obj);
 	}
