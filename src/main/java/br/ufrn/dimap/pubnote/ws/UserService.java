d70a4a9b1d53e517fd6c739c205f36c45894577d
judf <judf.oliveira@gmail.com>
2013-05-13 11:04:01 -0300

    Adicionando mais serviços.

--- src/main/java/br/ufrn/dimap/pubnote/ws/UserService.java
+++ src/main/java/br/ufrn/dimap/pubnote/ws/UserService.java
@@ -1,22 +1,33 @@
 package br.ufrn.dimap.pubnote.ws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.hibernate.Transaction;
 
+import br.ufrn.dimap.pubnote.dao.EvaluationDAO;
+import br.ufrn.dimap.pubnote.dao.EvaluationDAOFactory;
 import br.ufrn.dimap.pubnote.dao.ProfileDAO;
 import br.ufrn.dimap.pubnote.dao.ProfileFactory;
 import br.ufrn.dimap.pubnote.dao.UserDAO;
 import br.ufrn.dimap.pubnote.dao.UserDAOFactory;
 import br.ufrn.dimap.pubnote.domain.Article;
 import br.ufrn.dimap.pubnote.domain.ArticleEntity;
+import br.ufrn.dimap.pubnote.domain.Evaluation;
+import br.ufrn.dimap.pubnote.domain.Friend;
+import br.ufrn.dimap.pubnote.domain.FriendEntity;
 import br.ufrn.dimap.pubnote.domain.Profile;
 import br.ufrn.dimap.pubnote.domain.ProfileEntity;
+import br.ufrn.dimap.pubnote.domain.TagEntity;
 import br.ufrn.dimap.pubnote.domain.User;
 import br.ufrn.dimap.pubnote.domain.UserEntity;
 import br.ufrn.dimap.pubnote.ws.responses.LoginResponse;
@@ -142,5 +153,84 @@ public class UserService {
 		
 		tx.commit();
 		return Response.status(201).build();
+	}
+
+	/**
+	 * curl -i  -H "Content-Type: application/json" -X POST -d '{"id":"6"}' http://localhost:8080/pubnote.server/rest/user/addFriends
+	 * 
+	 * @param user
+	 * @return
+	 */
+	@POST
+	@Path("/addFriends")
+	public Response addFriendsOfUser(User user){
+		userDAO = userFactory.createDAO();
+		List<Friend> friends = new ArrayList<Friend>();
+
+		for (int i = 0; i < user.getFriends().size(); i++) {
+			friends.add(user.getFriends().get(i));
+		}
+		Transaction tx = userDAO.beginTransaction();
+		UserEntity entity = userDAO.load(user.getId());
+		
+		for (int i = 0; i < friends.size(); i++) {
+			FriendEntity friend = (FriendEntity) userDAO.load(friends.get(i).getId());
+			if(friend.getId() == 0){
+				//sem tag
+				friend = new FriendEntity();
+				userDAO.persist(friend);
+			}
+			
+			entity.getFriends().add(friend);
+		}
+
+		userDAO.update(entity);
+		
+		tx.commit();
+		return Response.status(201).build();
+	}
+	
+	/**
+	 * curl -i   -H "Content-Type: application/json" -X GET -d '{"username":"juliana"}' http://localhost:8080/pubnote.server/rest/user/getPeople
+	 * @param user
+	 * @return
+	 */	
+	@GET
+	@Path("/getPeople")
+	public User[] retrieveUsers(@QueryParam("username") String username)
+	{
+		userDAO = userFactory.createDAO();
+		//Transaction tx = evalDao.beginTransaction();
+		User[] users = userDAO.retrieveUsers(username);
+		//evalDao.commit(tx);
+		return users;
+	}
+	
+	
+	/**
+	 * curl -i   -H "Content-Type: application/json" -X POST -d '{"username":"juliana"}' http://localhost:8080/pubnote.server/rest/user/getFriends
+	 * @param user
+	 * @return
+	 */	
+	@POST
+	@Path("/getFriends")
+	public User[] retrieveFriends(User user)
+	{
+		userDAO = userFactory.createDAO();
+		Transaction tx = userDAO.beginTransaction();
+
+		UserEntity entity = userDAO.loadByUsername(user.getUsername());
+		List<FriendEntity> friends = entity.getFriends();
+		
+		User[] userArray = new User[friends.size()];
+		if(friends.size() != 0){	
+			for(int i = 0; i < userArray.length; i++)
+			{
+				userArray[i] = friends.get(i).convertToUser();
+			}
+		}
+		
+		userDAO.commit(tx);
+		return userArray;
 	}
 }
